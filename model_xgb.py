c053d9a4316fc192d2011f50b7875acbcdcc7ad7
Yabo Li <liyaboleo@hotmail.com>
2017-09-28 16:37:11 -0400

    initial commit

---
+++ model_xgb.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+import pandas as pd
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.feature_selection import SelectFromModel
+from sklearn.preprocessing import label_binarize
+from sklearn.preprocessing import normalize
+from scipy import interp
+from scipy.stats import ks_2samp
+import collections
+import time
+
+
+path = 'D:/'
+file_name = 'data.csv'
+
+# load data
+data = pd.read_csv(path + file_name)
+
+# extract 'Order_Qty' column to be used as labels
+y = data['Order_Qty'].values
+# replace all non-zero values with one
+y[y > 0] = 1
+# total number of data entries
+data_size = len(y)
+dict_order_qty = dict(collections.Counter(y))
+keys_order_qty = list(dict_order_qty.keys())
+for i in range(0, len(keys_order_qty)):
+    key = keys_order_qty[i]
+    dict_order_qty[key] = dict_order_qty.get(key) / data_size
+
+# extract 'Country' column
+countries = data['Country'].values
+# compute frequency of each country
+dict_country = dict(collections.Counter(countries))
+keys_country = list(dict_country.keys())
+for i in range(0, len(keys_country)):
+    key = keys_country[i]
+    dict_country[key] = dict_country.get(key) / data_size
+
+# extract 'SKU' column
+skus = data['SKU'].values
+# compute frequency of each SKU
+dict_sku = dict(collections.Counter(skus))
+keys_sku = list(dict_sku.keys())
+for i in range(0, len(keys_sku)):
+    key = keys_sku[i]
+    dict_sku[key] = dict_sku.get(key) / data_size
+
+# extract 'SKU_Category' column
+sku_categories = data['SKU_Category'].values
+# compute frequency of each SKU category
+dict_sku_category = dict(collections.Counter(sku_categories))
+keys_sku_category = list(dict_sku_category.keys())
+for i in range(0, len(keys_sku_category)):
+    key = keys_sku_category[i]
+    dict_sku_category[key] = dict_sku_category.get(key) / data_size
+
+# extract 'EB_Flag' column
+eb_flags = data['EB_Flag'].values
+# compute frequency of each EB_Flag
+dict_eb_flag = dict(collections.Counter(eb_flags))
+keys_eb_flag = list(dict_eb_flag.keys())
+for i in range(0, len(keys_eb_flag)):
+    key = keys_eb_flag[i]
+    dict_eb_flag[key] = dict_eb_flag.get(key) / data_size
+
+# extract 'RFQ_TYPE' column
+rfq_types = [float(i) for i in data['RFQ_TYPE'].values]
+# compute frequency of each RFQ_TYPE
+dict_rfq_type = dict(collections.Counter(rfq_types))
+keys_rfq_type = list(dict_rfq_type.keys())
+for i in range(0, len(keys_rfq_type)):
+    key = keys_rfq_type[i]
+    dict_rfq_type[key] = dict_rfq_type.get(key) / data_size
+rfq_types_b = label_binarize(rfq_types, classes=range(0, 9))
+
+print('\nOne-hot encoded samples (RFQ_Type):')
+print('3: {}'.format(rfq_types_b[10]))
+print('5: {}'.format(rfq_types_b[29839]))
+print('7: {}'.format(rfq_types_b[29997]))
+print('8: {}'.format(rfq_types_b[30005]))
+print('2: {}'.format(rfq_types_b[30112]))
+print('6: {}'.format(rfq_types_b[119100]))
+print('4: {}'.format(rfq_types_b[136174]))
+print('9: {}'.format(rfq_types_b[143901]))
+print('1: {}'.format(rfq_types_b[154238]))
+
+file = open('result.txt', 'w')
+file.write("Country:")
+file.write('\nKeys: {}'.format(keys_country))
+file.write('\nNo. of keys: {}'.format(len(keys_country)))
+file.write('\nPercentage of each category: {}'.format(dict_country))
+file.write("\n\nSKU:")
+file.write('\nKeys: {}'.format(keys_sku))
+file.write('\nNo. of keys: {}'.format(len(keys_sku)))
+file.write('\nPercentage of each category: {}'.format(dict_sku))
+file.write("\n\nSKU_Category:")
+file.write('\nKeys: {}'.format(keys_sku_category))
+file.write('\nNo. of keys: {}'.format(len(keys_sku_category)))
+file.write('\nPercentage of each category: {}'.format(dict_sku_category))
+file.write("\n\nEB_Flag:")
+file.write('\nKeys: {}'.format(keys_eb_flag))
+file.write('\nNo. of keys: {}'.format(len(keys_eb_flag)))
+file.write('\nPercentage of each category: {}'.format(dict_eb_flag))
+file.write("\n\nRFQ_Type:")
+file.write('\nKeys: {}'.format(keys_rfq_type))
+file.write('\nNo. of keys: {}'.format(len(keys_rfq_type)))
+file.write('\nPercentage of each category: {}'.format(dict_rfq_type))
+file.write('\nOne-hot Encoded RFQ_Type:\n{}'.format(rfq_types_b))
+file.write("\n\nOrder_Qty:")
+file.write('\nKeys: {}'.format(keys_order_qty))
+file.write('\nNo. of keys: {}'.format(len(keys_order_qty)))
+file.write('\nPercentage of each category: {}'.format(dict_order_qty))
+file.close()
